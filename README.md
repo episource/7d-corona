beeef2e19039f60ef7f5d42545a2da6ebe7fc338
Philipp Serr <episource@gmx.de>
2020-07-26 10:32:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Visualization of new covid-19 cases within last 7 days for selected contries
+The `update-xlsx.ps1` script collects covid-19 data from the `European Centre for Disease Prevention and Control` (`ECDC`) and updates the `7d-corona.xlsx` file accordingly. Excel is used for easy visualization of the data.
+
+Intent: A quarantine is currently required when returning to germany if in the country of origin, there have been more than 50 new infections per 100k inhabitants within the last seven days. German RKI does provide a prebuilt plot of this metric.
+
+Keep in mind: the inofficial calculations used by this repository are not relevant for officials, but the data gathered by RKI! Therefore, the data visualized by `7d-corona.xlsx` can only be an indicator! **Ultimately relevant are the [values published by german RKI](https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Risikogebiete_neu.html) and the state of living (e.g. [Baden-Württemberg](https://sozialministerium.baden-wuerttemberg.de/de/gesundheit-pflege/gesundheitsschutz/infektionsschutz-hygiene/informationen-zu-coronavirus/verordnungen/risikogebiete/)).**
+
+
+## Data sources
+## European Centre for Disease Prevention and Control (ECDC)
+ - Covid-19 cases are extracted from [ecdc's csv downloadable data set](https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide)
+ - population data for 2018 contained in above csv data set is used for calculations
+
+## Requirements
+ - Powershell v5
+ - Excel 2016 (other versions are likely to work as well)
+ 
+## How To
+ 1. Make sure `7d-corona.xlsx` is not opened
+ 2. Run powershell script `update-xlsx.ps1`
+ 3. Open `7d-corona.xlsx` to view the collected data
+ 
+## Git Setup
+Before commiting to this repository, [_setup.sh](_setup.sh) should be run to
+setup a [git smudge filter](https://www.git-scm.com/docs/gitattributes#_filter),
+that decompresses `xlsx` files during commit: `xlsx` files are zip compressed
+archives of mostly `xml` formatted plaintext files. Git's delta compression
+works very well for xml files, but not if compressed. For security reasons, the
+filter needs to be configured explicitly. Therefore, it cannot be shared in a
+way that does need user interaction. Decompressing is done using compression
+level `store` which preserves full FreeCAD compatibility. A clean filter is not
+used, therefore files in working tree are slightly bigger after checkout
+compared with the initial file size.
---
+++ update-xlsx.ps1
@@ -0,0 +1,135 @@
+﻿# Copyright 2020 Philipp Serr (episource)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Enable common parameters
+[CmdletBinding()] 
+Param(
+)
+
+$xlsx = $PSScriptRoot + "\7d-corona.xlsx"
+$imgFile = $PSScriptRoot + "\7d-corona.png"
+$imgWidth = $None
+
+$sheetChart = "Schaubild"
+$chartName = "Neuinfektionen"
+$sheetCountryMap = @{
+    "ECDC Deutschland" = "germany"
+    "ECDC Frankreich" = "france"
+    "ECDC Spanien" = "spain"
+    "ECDC Österreich" = "austria"
+    "ECDC Schweiz" = "switzerland"
+}
+
+
+$queryUrlEcdc = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
+
+
+function Update-EcdcSheet($sheet, $data, $country, $lastUpdate) {
+    $curRow = 3
+    $data | ?{
+        $_.country -eq $country 
+    } | %{
+        $curRow++
+        $sheet.Cells.Item($curRow, 1) = $_.date.ToOADate()
+        $sheet.Cells.Item($curRow, 2) = $_.cases
+        $sheet.Cells.Item($curRow, 4) = $_.population
+    }
+    
+    $sheet.Cells.Item(1, 1) = $lastUpdate
+}
+
+function Get-DataFromEcdc() {
+    $r = Invoke-WebRequest -UseBasicParsing $queryUrlEcdc
+    
+    return [String]::new($r.Content) | %{
+        $_ -split "[\r\n]+" 
+    } | Select-Object -Skip 1 | ?{
+        $_.Length -gt 0
+    } | %{
+        $row = $_.Split(",")
+        [PSCustomObject]@{ 
+            "date"=[DateTime]::ParseExact($row[0], "dd/MM/yyyy", $null)
+            "cases"=$row[4]
+            "country"=$row[6]
+            "population"=$row[9]
+        }
+    } 
+}
+
+$now = Get-Date -format "dddd yyyy-MM-dd HH:mm"
+$lastUpdate = "Datenabruf: $now"
+
+$excel = New-Object -ComObject Excel.Application
+try {
+    $excel.Visible = $true
+    $excel.ScreenUpdating = $False 
+    $excelWb = $excel.Workbooks.Open($xlsx)
+    $excelSheetChart = $excelWb.Sheets($sheetChart)
+    
+    
+    $ecdcData = Get-DataFromEcdc
+    
+    $sheetCountryMap.Keys | %{
+        $excelSheet = $excelWb.Sheets($_)
+        Update-EcdcSheet $excelSheet $ecdcData $sheetCountryMap[$_] $lastUpdate
+    }
+    
+    
+    $excelChart = $excelSheetChart.ChartObjects($chartName)
+    $excelChartXValues = $excelChart.Chart.SeriesCollection(1).XValues
+    
+    $wc = [Math]::floor($excelChartXValues.Length / 7)
+    $lastOaDate = $excelChartXValues.Get(1)
+    $lastDate = [System.DateTime]::FromOaDate($lastOaDate)
+    $daysToNextMonday = (8 - $lastDate.DayOfWeek) % 7
+    
+    $excelChart.Chart.Axes(1).MinimumScale = $lastOaDate + $daysToNextMonday - $wc * 7
+    $excelChart.Chart.Axes(1).MaximumScale = $lastOaDate + $daysToNextMonday
+    $excelChart.Chart.ChartTitle.Text = "Neuinfektionen/100k (7 Tage) - $lastUpdate"
+    
+    $excel.ScreenUpdating = $True
+    
+    try {
+        $excelChart.CopyPicture([Microsoft.Office.Interop.Excel.XlPictureAppearance]::xlScreen, [Microsoft.Office.Interop.Excel.XlCopyPictureFormat]::xlBitmap)
+        
+        $img = Get-Clipboard -Format Image
+        if (-not $img) {
+            throw "clipboard empty"
+        }
+
+        if ($imgWidth -eq $None) {
+            $imgWidth = $img.Width
+        }
+        $imgHeight = [int]($img.Height / $img.Width * $imgWidth)
+        $outBitmap = New-Object -TypeName System.Drawing.Bitmap -ArgumentList $imgWidth, $imgHeight
+        
+        $outG = [System.Drawing.Graphics]::FromImage($outBitmap)
+        $outG.SmoothingMode = "HighQuality"
+        $outG.InterpolationMode = "HighQualityBicubic"
+        $outG.PixelOffsetMode = "HighQuality"
+        $outGRectangle = 
+        $outG.DrawImage($img, [System.Drawing.Rectangle]::new(0, 0, $imgWidth, $imgHeight))
+        
+        $outBitmap.Save($imgFile)
+    } catch {
+        Write-Warning "Failed to save chart image: $_"
+        throw
+    }
+    
+    $excelWb.Save()
+} finally {
+    $excel.ScreenUpdating = $True
+    $excel.Quit()
+}
+
